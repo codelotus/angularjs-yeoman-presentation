aa159e64993d17f440fb4e842ea961ba73c5ca26
Brian Ehmann <behmann@gmail.com>
2013-03-05 22:01:42 -0700

    beginning tests

--- code/test/spec/controllers/newBook.js
+++ code/test/spec/controllers/newBook.js
@@ -17,6 +17,6 @@ describe('Controller: NewBookCtrl', function() {
   }));
 
   it('should attach a list of awesomeThings to the scope', function() {
-    expect(scope.awesomeThings.length).toBe(3);
+    expect(scope.title.length).toBe(0);
   });
 });

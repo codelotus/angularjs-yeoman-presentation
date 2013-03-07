96f3f17e153c8415f0f3b64a4dbf9342cfa012ef
Brian Ehmann <behmann@gmail.com>
2013-03-06 20:55:03 -0700

    added show notes

--- code/app/scripts/controllers/book.js
+++ code/app/scripts/controllers/book.js
@@ -6,6 +6,6 @@ codeApp.controller('BookCtrl', function($scope, $http, $routeParams, localStorag
   var bookId = $routeParams.bookId;
   var bookJson = localStorageService.get(bookId);
   $scope.book = JSON.parse(bookJson);
-  $log.log("Book: " + $scope.book);
+  $log.log("Book: " + JSON.stringify($scope.book));
 
 });
